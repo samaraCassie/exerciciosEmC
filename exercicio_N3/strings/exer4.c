d8a9a8dcc3569ffffb1256c0ceec08ce1fd0c3ef
samaraCassie <samaracassie@outlook.com>
2023-07-03 16:02:50 -0300

    feat: exercise of string

--- exercicio_N3/strings/exer4.c
+++ exercicio_N3/strings/exer4.c
@@ -14,48 +14,60 @@
  * dentro da string S e retorne se encontrou ou não como 
  * resultado (mesma lógica da letra “g” da questão 3). 
  */
-int searchString(char S[], char subString[]) {
+int searchSubstring(char string[], char substring[]) {
     int i = 0, j = 0;
-    int search = 0;
+    int found = 0;
 
-    while (S[i] != '\0') {
-        if (S[i] == subString[j]) {
+    while (string[i] != '\0') {
+        if (string[i] == substring[j]) {
             j++;
-            if (subString[j] == '\0') search = 1;
-        } else j = 0;
+            if (substring[j] == '\0') {
+                found = 1;
+                break;
+            }
+        } else {
+            j = 0;
+        }
         i++;
     }
-    return search;
+    return found;
 }
 
 /**Calcule e exiba o tamanho/quantidade 
  * de caracteres da frase/String (mesma 
  * lógica da letra “f” da questão 3).
 **/
-
-int sizeString(char S[]) {
-    int sizeString = 0;
+int calculateStringLength(char S[]) {
+    int lengthString = 0;
     for (int i = 0; S[i] != '\0'; i++) {
-        sizeString++;
+        lengthString++;
     }
-    return sizeString;
+    return lengthString;
 }
 
-int compareStrings(char S1[], char S2[]) {
+//Informe uma segunda frase e faça a comparação se a frase é igual à frase contida na string S.
+int compareStrings(char string1[], char string2[]) {
     int i = 0;
-    while (S1[i] != '\0' && S2[i] != '\0') {
-        if (S1[i] != S2[i]) {
+    while (string1[i] != '\0' || string2[i] != '\0') {
+        if (string1[i] != string2[i]) {
             return 0;
         }
         i++;
     }
-    if (S1[i] == '\0' && S2[i] == '\0') {
+    if (string1[i] == '\0' && string2[i] == '\0') {
         return 1;
     } else {
         return 0;
     }
 }
 
+/**
+ * Substitua o valor da string S por “catolica” 
+ * (não fucionará S = “catolica”, então deverá 
+ * substituir letra a letra através e adicionar 
+ * ‘\0’ ao final. Após, testar o tamanho da nova 
+ * string com a lógica da letra “b”.
+**/
 void replaceString(char S[]) {
     char newString[] = "catolica";
     int i = 0;
@@ -69,20 +81,13 @@ void replaceString(char S[]) {
 int main() {
     char S[MAX];
     printf("Digite uma frase: ");
-    fgets(S, MAX, stdin);
-
-    // Remover o caractere '\n' do final da string lida por fgets
-    int i = 0;
-    while (S[i] != '\n') {
-        i++;
-    }
-    S[i] = '\0';
+    scanf("%s", &S[MAX]);
 
     // (a) Busca por uma palavra ou parte dela
     char subString[MAX];
     printf("Digite uma palavra ou sub-string para buscar: ");
-    fgets(subString, MAX, stdin);
-    int search = searchString(S, subString);
+    scanf("%s", &subString[MAX]);
+    int search = searchSubstring(S, subString);
     if (search) {
         printf("A sub-string foi encontrada na frase.\n");
     } else {
@@ -90,25 +95,25 @@ int main() {
     }
 
     // (b) Calcular e exibir o tamanho da frase
-    int size = sizeString(S);
-    printf("Tamanho da frase: %d\n", size);
+    int length = calculateStringLength(S);
+    printf("Tamanho da frase: %d\n", length);
 
     // (c) Comparar com uma segunda frase
     char segundaFrase[MAX];
     printf("Digite uma segunda frase para comparar: ");
-    fgets(segundaFrase, MAX, stdin);
-    int igual = compareStrings(S, segundaFrase);
-    if (igual) {
-        printf("As frases são iguais.\n");
+    scanf("%s", &segundaFrase[MAX]);
+    int result = compareStrings(S, segundaFrase);
+    if (result) {
+        printf("As frases sao iguais.\n");
     } else {
-        printf("As frases são diferentes.\n");
+        printf("As frases sao diferentes.\n");
     }
 
     // (d) Substituir a string por "catolica"
-    substituiString(S);
-    size = sizeString(S);
+    replaceString(S);
+    length = calculateStringLength(S);
     printf("Nova frase: %s\n", S);
-    printf("Tamanho da nova frase: %d\n", size);
+    printf("Tamanho da nova frase: %d\n", length);
 
     return 0;
 }
