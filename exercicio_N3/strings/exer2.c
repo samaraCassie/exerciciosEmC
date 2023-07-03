7731df7fd0c07a809928d6bc246d3730dc330f42
samaraCassie <samaracassie@outlook.com>
2023-07-02 22:17:15 -0300

    feat: exercise of string

---
+++ exercicio_N3/strings/exer2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Construa um algoritmo que leia um vetor de 10 caracteres, 
+e diga quantas consoantes foram lidas. Imprima as consoantes.
+
+@samaraCassie
+*/
+
+int main(){
+    
+    int soma=0; 
+    char vetor[10] = {'d', 'o', 'g', 'u', 'i', 'n', 'h', 'o', 's', '\0'};
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (vetor[i] != '\0'){
+            if (vetor[i]!= 'a' && vetor[i]!= 'e' && vetor[i]!= 'i' && vetor[i]!= 'o' && vetor[i]!= 'u'){
+                soma++;
+                printf("consoantes: %c\n", vetor[i]);
+            }
+        }
+    }
+    printf("Qnt de consoantes: %d", soma);
+}
