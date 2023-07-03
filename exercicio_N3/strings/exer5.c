dc3f35f33a4da02d3a3c4c375b338300c020aafa
samaraCassie <samaracassie@outlook.com>
2023-07-03 13:46:09 -0300

    feat: exercise of string

--- exercicio_N3/strings/exer5.c
+++ exercicio_N3/strings/exer5.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-/*
-Construa um algoritmo que leia (scanf) 
-um vetor de 5 números inteiros e mostre a
-soma dos números e os valores do vetor. 
-(Use forma contraída para realizar a soma).
+#define MAX 20
 
-@samaraCassie
-*/
+/**@samaraCassie
+ * 
+ * Criar um programa que leia uma palavra[20]  
+ * com fgets(), guarde na variável palavra e 
+ * imprima na tela o seguinte texto (tudo na 
+ * mesma linha):
+ * 
+ * Saída ---->   “Palavra <palavra>  lida com fgets()”. 
+**/
 
-int main()
-{
-    int vet[5], sum=0;
+int main() {
+    char string[MAX];
+    printf("Digite uma palavra: ");
+    fgets(string, MAX, stdin);
 
-    for (int i = 0; i < 5; i++){
-        printf("Insira um numero inteiro\n");
-        scanf("%d", &vet[i]);
-        sum+=vet[i];
-    }
-    printf("-------------------------------");
-    printf("\n\nA soma dos valores eh: %d\n", sum);
-    printf("\nVetor:\n [ ");
-    for(int i = 0; i < 5; i++) printf("%d ", vet[i]); 
-    printf("]");   
+    string[strcspn(string, "\n")] = '\0';
+
+    printf("Saida ----> \"Palavra %s lida com fgets().\"\n", string);
+
+    return 0;
 }
